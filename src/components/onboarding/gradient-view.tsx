a3f37abf6c8e7ec01949c3c95a0b0836e4f63b3f
Rishab Khivsara <rishabjain3500@gmail.com>
2025-01-27 20:18:50 +0530

    New components

---
+++ src/components/onboarding/gradient-view.tsx
@@ -0,0 +1,32 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { type ReactNode } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { tv } from 'tailwind-variants';
+
+import { Hashtag } from '@/components/ui/icons/hashtag';
+
+type TGradientViewProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+const gradientView = tv({
+  base: 'flex grow justify-between',
+});
+
+export default function GradientView({
+  children,
+  className,
+}: TGradientViewProps) {
+  return (
+    <LinearGradient
+      colors={['#DFE8FF', '#FFFFFF']}
+      style={{ flex: 1, flexGrow: 1 }}
+    >
+      <SafeAreaView className={gradientView({ className })}>
+        {children}
+        <Hashtag className="absolute bottom-0 right-0 z-[-1]" />
+      </SafeAreaView>
+    </LinearGradient>
+  );
+}
