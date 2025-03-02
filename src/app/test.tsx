85056f01c8171b647baf3c62c093a0a62be62bd9
Rishab Khivsara <rishabjain3500@gmail.com>
2025-03-02 14:20:50 +0530

    Add new components to test screen

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,63 +1,142 @@
+import { CalendarDays } from 'lucide-react-native';
+import React from 'react';
+import { ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Button, ButtonIcon, ButtonText } from '@/components/ui';
 
 import { Typography } from '../components/ui/text';
 
 const Test = () => {
   return (
     <SafeAreaView className="m-5 grow">
-      <Typography type="heading" className="text-error">
-        Heading
-      </Typography>
-      <Typography type="heading" color="green">
-        Heading Green
-      </Typography>
-      <Typography type="heading" color="green" className="text-purple-200">
-        Heading Overridden
-      </Typography>
-      <Typography type="subtext">Subtext</Typography>
-      <Typography type="subtext" color="primary">
-        Subtext Primary
-      </Typography>
-      <Typography type="paragraph">Paragraph</Typography>
-      <Typography type="paragraph" color="error">
-        Paragraph Error
-      </Typography>
-      <Typography type="label">Label</Typography>
-      <Typography type="label" color="main">
-        Label Main
-      </Typography>
-      <Typography type="placeholder">Placeholder</Typography>
-      <Typography type="placeholder" color="primary">
-        Placeholder Primary
-      </Typography>
+      <ScrollView>
+        <View>
+          <Typography type="heading" className="text-error">
+            Heading
+          </Typography>
+          <Typography type="heading" color="green">
+            Heading Green
+          </Typography>
+          <Typography type="heading" color="green" className="text-purple-200">
+            Heading Overridden
+          </Typography>
+          <Typography type="subtext">Subtext</Typography>
+          <Typography type="subtext" color="primary">
+            Subtext Primary
+          </Typography>
+          <Typography type="paragraph">Paragraph</Typography>
+          <Typography type="paragraph" color="error">
+            Paragraph Error
+          </Typography>
+          <Typography type="label">Label</Typography>
+          <Typography type="label" color="main">
+            Label Main
+          </Typography>
+          <Typography type="placeholder">Placeholder</Typography>
+          <Typography type="placeholder" color="primary">
+            Placeholder Primary
+          </Typography>
+        </View>
 
-      <Typography weight={100} className="text-[36px]">
-        The quick brown fox
-      </Typography>
-      <Typography weight={200} className="text-[36px]">
-        The quick brown fox
-      </Typography>
-      <Typography weight={300} className="text-[36px]">
-        The quick brown fox
-      </Typography>
-      <Typography weight={400} className="text-[36px]">
-        The quick brown fox
-      </Typography>
-      <Typography weight={500} className="text-[36px]">
-        The quick brown fox
-      </Typography>
-      <Typography weight={600} className="text-[36px]">
-        The quick brown fox
-      </Typography>
-      <Typography weight={700} className="text-[36px]">
-        The quick brown fox
-      </Typography>
-      <Typography weight={800} className="text-[36px]">
-        The quick brown fox
-      </Typography>
-      <Typography weight={900} className="text-[36px]">
-        The quick brown fox
-      </Typography>
+        <View>
+          <Typography weight={100} className="text-[36px]">
+            The quick brown fox
+          </Typography>
+          <Typography weight={200} className="text-[36px]">
+            The quick brown fox
+          </Typography>
+          <Typography weight={300} className="text-[36px]">
+            The quick brown fox
+          </Typography>
+          <Typography weight={400} className="text-[36px]">
+            The quick brown fox
+          </Typography>
+          <Typography weight={500} className="text-[36px]">
+            The quick brown fox
+          </Typography>
+          <Typography weight={600} className="text-[36px]">
+            The quick brown fox
+          </Typography>
+          <Typography weight={700} className="text-[36px]">
+            The quick brown fox
+          </Typography>
+          <Typography weight={800} className="text-[36px]">
+            The quick brown fox
+          </Typography>
+          <Typography weight={900} className="text-[36px]">
+            The quick brown fox
+          </Typography>
+        </View>
+
+        <View className="gap-4">
+          <Button className="items-stretch">
+            <View className="flex justify-center ">
+              <CalendarDays className="w-8 text-white" />
+            </View>
+            <ButtonText weight={100} className="text-[36px] text-white">
+              Hello world
+            </ButtonText>
+          </Button>
+
+          <Button>
+            <ButtonIcon>
+              <CalendarDays className="w-8 text-white" />
+            </ButtonIcon>
+            <ButtonText weight={400} className="text-[26px] text-white">
+              Hello world
+            </ButtonText>
+          </Button>
+
+          <Button className="items-center">
+            <View className="flex-row items-center gap-3">
+              <CalendarDays className="w-8 text-white" />
+              <ButtonText
+                weight={100}
+                className="text-[36px] text-white"
+                style={{
+                  includeFontPadding: false,
+                }}
+              >
+                žHello world
+              </ButtonText>
+            </View>
+          </Button>
+
+          <Button>
+            <ButtonIcon>
+              <CalendarDays className="w-8 text-white" />
+            </ButtonIcon>
+            <ButtonText weight={400} className="text-[26px] text-white">
+              Hello world
+            </ButtonText>
+          </Button>
+
+          <Button>
+            <ButtonText weight={900} className="text-[36px] text-white">
+              The quick fox
+            </ButtonText>
+          </Button>
+
+          <Button>
+            <ButtonText weight={500} className="text-[26px] text-white">
+              The quick fox
+            </ButtonText>
+          </Button>
+
+          <Button>
+            <ButtonText weight={600} className="text-[14px] text-white">
+              The quick fox
+            </ButtonText>
+          </Button>
+
+          <Button>
+            <ButtonText weight={500} className="text-[20px] text-white">
+              The quick fox
+            </ButtonText>
+          </Button>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
