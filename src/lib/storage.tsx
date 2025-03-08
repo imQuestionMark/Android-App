635a6f492ee3fe393ed26529d3211120f69cdb19
Rishab Khivsara <rishabjain3500@gmail.com>
2025-03-08 22:09:30 +0530

    Add secureStore utils

--- src/lib/storage.tsx
+++ src/lib/storage.tsx
@@ -1,3 +1,4 @@
+import { deleteItemAsync, getItemAsync, setItemAsync } from 'expo-secure-store';
 import { MMKV } from 'react-native-mmkv';
 
 export const storage = new MMKV();
@@ -13,4 +14,16 @@ export async function setItem<T>(key: string, value: T) {
 
 export async function removeItem(key: string) {
   storage.delete(key);
+}
+
+export async function getItemSecurely(key: string) {
+  return await getItemAsync(key);
+}
+
+export async function setItemSecurely<T>(key: string, value: string) {
+  await setItemAsync(key, value);
+}
+
+export async function removeItemSecurely(key: string) {
+  await deleteItemAsync(key);
 }
