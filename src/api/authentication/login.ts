f5d541fd0c58129f310c6cc35d65defc5d171f1d
Rishab Khivsara <rishabjain3500@gmail.com>
2025-03-08 18:29:22 +0530

    Add router param to help with auth flow

--- src/api/authentication/login.ts
+++ src/api/authentication/login.ts
@@ -6,6 +6,7 @@ import { saveUserID } from '@/lib/store/user-store';
 import { API_ROUTES } from '@/routes/api-routes';
 
 import { client } from '../common';
+import { devLog } from '@/lib/utils';
 
 export const loginInputSchema = z.object({
   identifier: z
@@ -16,25 +17,29 @@ export const loginInputSchema = z.object({
 export type Variables = z.infer<typeof loginInputSchema>;
 
 const loginResponseSchema = z.object({
+  status: z.number(),
+  message: z.string({ required_error: 'Message is missing in the Response' }),
   data: z.object({
     id: z.string({ required_error: 'ID is missing in the Response' }),
   }),
-  message: z.string({ required_error: 'Message is missing in the Response' }),
 });
 
 type Response = z.infer<typeof loginResponseSchema>;
 
-const submitForm = async (data: Variables) => {
+const loginRequest = async (data: Variables) => {
   const response = await client.post(API_ROUTES.LOGIN, data);
   return loginResponseSchema.parse(response.data);
 };
 
 export const useLoginMutation = createMutation<Response, Variables, Error>({
-  mutationFn: submitForm,
-  onSuccess: (data) => {
-    console.log('Valid email:', data);
+  mutationFn: loginRequest,
+  onSuccess: async (data) => {
+    devLog('Valid email:', data);
     // @INFO Saving the userID in expo-secure-store.
-    saveUserID(data.data.id);
-    router.replace({ pathname: '/verification' });
+    await saveUserID(data.data.id);
+    router.replace({
+      pathname: '/verification',
+      params: { entryPoint: 'login' },
+    });
   },
 });
--- src/api/authentication/signup.ts
+++ src/api/authentication/signup.ts
@@ -6,6 +6,7 @@ import { saveFirstName, saveUserID } from '@/lib/store/user-store';
 import { API_ROUTES } from '@/routes/api-routes';
 
 import { client } from '../common';
+import { devLog } from '@/lib/utils';
 
 const _DANGEROUS_INTERNAL_TENANT_ID = 'uCp1eujFVA';
 
@@ -18,7 +19,9 @@ export const SignUpInputschema = z.object({
     .string({ required_error: 'LastName is required' })
     .min(3, 'Minimum 3 characters')
     .max(16, 'Maximum 16 characters'),
-  emailAddress: z.string({ required_error: 'Email is required' }).email(),
+  emailAddress: z
+    .string({ required_error: 'Email is required' })
+    .email({ message: 'Incorrect Mail id' }),
   phone: z
     .string({ required_error: 'Phone no. is required' })
     .regex(/^(?:\+91|91)?[6-9]\d{9}$/, {
@@ -30,16 +33,17 @@ export type Variables = z.infer<typeof SignUpInputschema>;
 
 const SignUpResponseschema = z.object({
   status: z.number(),
-  message: z.string(),
+  message: z.string({ required_error: 'Message is missing in the Response' }),
   data: z.object({
-    id: z.string(),
+    id: z.string({ required_error: 'ID is missing in the Response' }),
   }),
 });
 
 type Response = z.infer<typeof SignUpResponseschema>;
 
 // @TODO Refactor using promise.all to run this in parallel.
-const submitForm = async (data: Variables) => {
+const signupRequest = async (data: Variables) => {
+  // @TODO: We are saving the firstName temporarily to show the name right away on personal-details screen. Ideally, there will be an API call I believe.
   await saveFirstName(data.firstName);
   const response = await client.post(API_ROUTES.SIGNUP, data, {
     headers: {
@@ -50,10 +54,14 @@ const submitForm = async (data: Variables) => {
 };
 
 export const useSignUpMutation = createMutation<Response, Variables, Error>({
-  mutationFn: submitForm,
+  mutationFn: signupRequest,
   onSuccess: async (data) => {
+    devLog('Sign up success, otp is sent for verification', data);
     // @INFO Saving the userID in expo-secure-store.
     await saveUserID(data.data.id);
-    router.replace({ pathname: '/(authentication)/verification' });
+    router.replace({
+      pathname: '/verification',
+      params: { entryPoint: 'signup' },
+    });
   },
 });
